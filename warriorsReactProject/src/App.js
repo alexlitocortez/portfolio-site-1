9d2757426f6bfc0edc7a165768454c332e234137
Alex Cortez <acim650@gmail.com>
2021-11-22 11:19:45 -0800

    warriorsReactProject

---
+++ warriorsReactProject/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WarriorStats from './warriorprojectTwo/WarriorStats copy';
+
+function App() {
+  return (
+    <div className="App">
+      <WarriorStats />
+    </div>
+  );
+}
+
+export default App;
